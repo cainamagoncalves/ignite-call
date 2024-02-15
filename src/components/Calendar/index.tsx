84e10d4625a131f7d8795204a5829afab25d9136
Cainã Gonçalves <moaraadrean@gmail.com>
2024-02-15 00:13:49 -0300

    fix: No unused selectedDate const

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -32,10 +32,7 @@ interface BlockedDates {
   blockedDates: number[]
 }
 
-export function Calendar({
-  selectedDate = null,
-  onDateSelected,
-}: CalendarProps) {
+export function Calendar({ onDateSelected }: CalendarProps) {
   const [currentDate, setCurrentDate] = useState(() => {
     return dayjs().set('date', 1)
   })
